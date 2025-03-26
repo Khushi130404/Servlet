e99f42912d0d18272a31d0251c24860022a1a2ff
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-03-26 10:06:33 +0530

    Servlet Setup

---
+++ BookServlet/src/main/java/BookDetailsServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/BookDetails")
+public class BookDetailsServlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+       
+    public BookDetailsServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
